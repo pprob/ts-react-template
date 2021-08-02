d97ed51ec9e1719f3b91f148dc4650e21df3d77b
unknown <p.probert340@gmail.com>
2021-08-02 16:26:53 +0100

    chore: wireup global reducer

--- src/client/redux/reducers.ts
+++ src/client/redux/reducers.ts
@@ -1,10 +1,12 @@
 import {History} from 'history';
 import {combineReducers} from 'redux';
 import {connectRouter} from 'connected-react-router';
+import globalReducer from './global/reducer';
 
 const createRootReducer = (history: History) => {
   const appReducers = {
     router: connectRouter(history),
+    global: globalReducer,
   };
   return combineReducers(appReducers);
 };
